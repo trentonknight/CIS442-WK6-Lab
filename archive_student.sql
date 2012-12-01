600b05e2bae2f503b8a5ecb8a93c74ce503d5f49
trentonknight <trentonknight@gmail.com>
2012-12-01 15:18:07 -0500

    almost there, one more function...

--- archive_student.sql
+++ archive_student.sql
@@ -3,17 +3,17 @@ DROP TABLE archiveStudent;
 CREATE TABLE archiveStudent
 (
   stdid            INT primary key,
-  fname            VARCHAR2(50) NOT NULL,
-  lname            VARCHAR2(50) NOT NULL,
+  fname            VARCHAR2(10) NOT NULL,
+  lname            VARCHAR2(13) NOT NULL,
   mi               VARCHAR2(1),
   majorid          VARCHAR2(15),
-  address          VARCHAR2(50),
-  city             VARCHAR2(50),
+  address          VARCHAR2(30),
+  city             VARCHAR2(20),
   state            VARCHAR2(2),
   zip              VARCHAR2(9),
   phone            VARCHAR2(15),
   advid            VARCHAR2(15),
-  change_user      VARCHAR2(50),
+  change_user      VARCHAR2(10),
   change_date      DATE
 );
 
--- createtabs.sql
+++ createtabs.sql
@@ -8,12 +8,12 @@ DROP TABLE student;
 CREATE TABLE student
 ( 
   stdid            INT  primary key,
-  fname            VARCHAR2(50) NOT NULL,
-  lname            VARCHAR2(50) NOT NULL,
+  fname            VARCHAR2(10) NOT NULL,
+  lname            VARCHAR2(13) NOT NULL,
   mi               VARCHAR2(1),
   majorid          VARCHAR2(15),
-  address          VARCHAR2(50),
-  city             VARCHAR2(50),
+  address          VARCHAR2(30),
+  city             VARCHAR2(20),
   state            VARCHAR2(2),
   zip              VARCHAR2(9),
   phone            VARCHAR2(15),
