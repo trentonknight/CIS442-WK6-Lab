634c8bd2e2d14794b1653f8e9ac8664457b0bae5
trentonknight <trentonknight@gmail.com>
2012-12-01 16:00:21 -0500

    some verbage added.

--- CIS442-WK6-MAIN.sql
+++ CIS442-WK6-MAIN.sql
@@ -1,3 +1,8 @@
+/*Jason N Mansfield REGIS CIS442*/
+/*WEEK 6 - LAB 6*/
+/*Please run this main script view Lab output only*/
+/*This main file will create all needed table, triggers and packages*/
+/*User running the script may need to modify paths to files if not run at root*/
 SPOOL CIS442-WK6-Lab6-SPOOL.txt
 DROP TRIGGER onDelete;
 DROP TRIGGER onInsert;
